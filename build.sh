0f0de8f700813b7c40a74b9e822811f1f86940ee
Lattn <lattnx@gmail.com>
2025-08-31 19:53:13 +0800

    feat: Update

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-xcaddy build --with github.com/caeret/caddy-ip-filter@v0.0.4 --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/replace-response
+xcaddy build --with github.com/caeret/caddy-ip-filter@v0.0.4 --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/replace-response --output caddy
+xcaddy build --with github.com/caeret/caddy-ip-filter@v0.0.4 --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/replace-response --with github.com/mholt/caddy-l4 --output caddyl4
